b10ceeb28ce259f6fec5684eeab139fa116e2137
Johannes Jordan <johannes.jordan@physiologie.uni-freiburg.de>
2015-11-24 17:13:29 +0100

    Add clickable behavior to table rows
    Builds upon datatables-select. As soon as a row is selected, the link fires

--- webroot/js/cakephp.dataTables.js
+++ webroot/js/cakephp.dataTables.js
@@ -11,6 +11,20 @@ var table = null;
 var oFilterTimerId = null;
 
 /**
+ * Add clickable behavior to table rows
+ * Builds upon datatables-select. As soon as a row is selected, the link fires
+ * Uses parameter rowLinkBase (e.g. controller + action link)
+ */
+function initRowLinks()
+{
+    table.api().on('select', function (e, dt, type, indexes) {
+        var rowData = table.api().rows(indexes).data();
+        var id = rowData[0].id;
+        window.location.href = params.rowLinkBase + '/' + id;
+    });
+}
+
+/**
  * Add search behavior to all search fields in column footer
  * Uses parameter 'delay' (milliseconds)
  */
