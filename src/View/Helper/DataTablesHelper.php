cdd965c542cfa4531e6d05ed5e882b2d3d99adb6
Johannes Jordan <johannes.jordan@physiologie.uni-freiburg.de>
2016-03-29 18:52:20 +0200

    Complete i18n strings

--- src/View/Helper/DataTablesHelper.php
+++ src/View/Helper/DataTablesHelper.php
@@ -26,19 +26,26 @@ class DataTablesHelper extends Helper
     {
         $this->_templater = $this->templater();
 
-        // -- load i18n
-        $foo = __d('data_tables', 'Processing...');
+        // -- load i18n (defaults from datatables.net/reference/option/language)
         $this->config('language', [
-            'paginate' => [
-                'next' => '<i class="fa fa-chevron-right"></i>',
-                'previous' => '<i class="fa fa-chevron-left"></i>'
-            ],
-            'processing' => __d('data_tables', 'Processing...'),
+            'emptyTable' => __d('data_tables', 'No data available in table'),
             'info' => __d('data_tables', 'Showing _START_ to _END_ of _TOTAL_ entries'),
-            'infoFiltered' => __d('data_tables', '(filtered from _MAX_ total entries)'),
             'infoEmpty' => __d('data_tables', 'No entries to show'),
+            'infoFiltered' => __d('data_tables', '(filtered from _MAX_ total entries)'),
+            'lengthMenu' => __d('data_tables', 'Show _MENU_ entries'),
+            'processing' => __d('data_tables', 'Processing...'),
             'search' => __d('data_tables', 'Search:'),
             'zeroRecords' => __d('data_tables', 'No matching records found'),
+            'paginate' => [
+                'first' => __d('data_tables', 'First'),
+                'last' => __d('data_tables', 'Last'),
+                'next' => __d('data_tables', 'Next'),
+                'previous' => __d('data_tables', 'Previous'),
+            ],
+            'aria' => [
+                'sortAscending' => __d('data_tables', ': activate to sort column ascending'),
+                'sortDescending' => __d('data_tables', ': activate to sort column descending'),
+            ],
         ]);
 
         // -- load user config (may overwrite i18n)
